2226d97d3d513b473285aec7130b77c714cfc7f1
Dwaynebok <dwaynebok19@gmail.com>
2020-10-20 11:24:31 +0200

    created a sweet class for our customers

---
+++ Model/Customer.php
@@ -0,0 +1,83 @@
+<?php
+
+
+class Customer
+{
+    private int $id;
+    private string $firstname;
+    private string $lastname;
+    private int $group_id;
+    private int $fixed_discount;
+    private int $variable_discount;
+
+    /**
+     * Customer constructor.
+     * @param int $id
+     * @param string $firstname
+     * @param string $lastname
+     * @param int $group_id
+     * @param int $fixed_discount
+     * @param int $variable_discount
+     */
+    public function __construct(int $id, string $firstname, string $lastname, int $group_id, int $fixed_discount, int $variable_discount)
+    {
+        $this->id = $id;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->group_id = $group_id;
+        $this->fixed_discount = $fixed_discount;
+        $this->variable_discount = $variable_discount;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGroupId(): int
+    {
+        return $this->group_id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFixedDiscount(): int
+    {
+        return $this->fixed_discount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVariableDiscount(): int
+    {
+        return $this->variable_discount;
+    }
+
+
+
+}
