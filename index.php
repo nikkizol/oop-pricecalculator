dfbf5ffbfadbb1aaae6c4bddc4cb8428acd7ce8d
MZ <nikita.zholkovskyi@gmail.com>
2020-10-20 10:08:07 +0200

    require model in index

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+require_once "Model/DatabaseConnection.php";
+require_once "Model/Product.php";
+require_once "Model/Product_Loader.php";
+
+//$getProducts = new Product_Loader();
+//$products = $getProducts->getProducts();
