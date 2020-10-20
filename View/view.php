55f08a7c5c688b151c4ebbf2110aee1b5b3cb190
Dwaynebok <dwaynebok19@gmail.com>
2020-10-20 10:02:57 +0200

    calculator view is done

---
+++ View/view.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Price Calculator</title>
+</head>
+<body>
+
+<form action="#" method="post">
+    <label for="price"></label>
+    <select name="price" id="priceCal">
+        <option value="volvo">Product</option>
+        <option value="saab">Discount</option>
+    </select>
+    <select name="price" id="priceCal">
+        <option value="volvo">First Name</option>
+        <option value="saab">Last Name</option>
+    </select>
+    <br><br>
+    <input type="submit" value="Submit">
+</form>
+
+
+</body>
+</html>
