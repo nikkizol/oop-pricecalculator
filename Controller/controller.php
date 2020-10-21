a5f146532dd13b91584a2db1681bc1078932f41a
MZ <nikita.zholkovskyi@gmail.com>
2020-10-21 09:22:13 +0200

    new day

--- Controller/controller.php
+++ Controller/controller.php
@@ -14,15 +14,6 @@ class controller
         $getCustomerGroup = new CustomerGroup_Loader();
         $customersGroup = $getCustomerGroup->getCustomerGroup();
 
-        function objArraySearch($array, $value)
-        {
-            foreach ($array as $customerGroup) {
-                if ($customerGroup->getId() == $value) {
-                    return $customerGroup;
-                }
-            }
-        }
-
         if (isset($_POST['name'])) {
             $groupId = $_POST['name'];
 
