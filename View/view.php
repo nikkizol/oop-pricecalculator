2bca44279c805940340ff7479710fca39adb8a38
Dwaynebok <dwaynebok19@gmail.com>
2020-10-22 15:45:10 +0200

    centered both customer and products

--- View/view.php
+++ View/view.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet"  href="/style.css" type="text/css">
+    <link rel="stylesheet" href="/style.css" type="text/css">
     <title>Price Calculator</title>
 </head>
 <body>
@@ -14,21 +14,27 @@
 <div class="box">
 
     <form method="post">
-        <label for="price" class="CN">Customer</label>
-        <select name="name" id="name">
+        <div class="CL">
+            <label for="name" id="labelName">Customer</label><br>
+            <select name="name" id="name">
+                <?php foreach ($customers as $customer): ?>
+                    <option value="<?php echo $customer->getGroupId(); ?>,<?php echo $customer->getFixedDiscount(); ?>,<?php echo $customer->getVariableDiscount(); ?>"> <?php echo $customer->getfirstName();
+                        echo " ", $customer->getLastName() ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
 
-            <?php foreach ($customers as $customer): ?>
-                <option value="<?php echo $customer->getGroupId(); ?>,<?php echo $customer->getFixedDiscount(); ?>,<?php echo $customer->getVariableDiscount(); ?>"> <?php echo $customer->getfirstName();
-                    echo " ", $customer->getLastName() ?></option>
-            <?php endforeach; ?>
-        </select>
-        <label for="price" class="PN">Product</label>
-        <select name="products" id="products">
-            <?php foreach ($products as $product): ?>  number_format($product->getPrice() / 100, 2)
-                <option value="<?php echo number_format($product->getPrice() / 100, 2); ?>"> <?php echo $product->getName();
-                    echo " ", number_format($product->getPrice() / 100, 2); ?></option>
-            <?php endforeach; ?>
-        </select>
+        <div class="CN">
+            <label for="products" id="labelProduct">Product</label><br>
+            <select name="products" id="products">
+                <?php foreach ($products as $product): ?>  number_format($product->getPrice() / 100, 2)
+                    <option value="<?php echo number_format($product->getPrice() / 100, 2); ?>"> <?php echo $product->getName();
+                        echo " ", number_format($product->getPrice() / 100, 2); ?></option>
+                <?php endforeach; ?>
+            </select>
+
+        </div>
+
 
         <br><?php echo "From: ", $whyDiscount;
         echo " ", $varDisc;
@@ -40,7 +46,7 @@
 
         <button class="Button">Submit</button>
 
-       <!-- <input type="submit" value="Submit">-->
+        <!-- <input type="submit" value="Submit">-->
 
     </form>
 </div>
